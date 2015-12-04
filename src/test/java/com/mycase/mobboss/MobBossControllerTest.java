34fd6f17ab077bb407c38bf54b83ee621402c10e
Anthony Agresta <anthony.agresta@mycase.com>
2015-12-04 11:24:52 -0800

    aa: Restructure to make Gradle project

---
+++ src/test/java/com/mycase/mobboss/MobBossControllerTest.java
@@ -0,0 +1,11 @@
+package com.mycase.mobboss;
+
+import static org.testng.Assert;
+import static org.testng.Annotations;
+
+public class MobBossControllerTest {
+    public static void someTest {
+
+    }
+
+}

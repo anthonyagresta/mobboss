1febb6c1c9b500811508ac5ff74c3fa113949531
Anthony Agresta <anthony.agresta@mycase.com>
2015-12-04 15:41:11 -0800

    aa: fix test

--- src/test/java/com/mycase/mobboss/MobBossControllerTest.java
+++ src/test/java/com/mycase/mobboss/MobBossControllerTest.java
@@ -1,12 +1,13 @@
 package com.mycase.mobboss;
 
-        import static org.testng.Assert;
-        import static org.testng.annotations.*;
+import org.testng.annotations.*;
+import org.testng.Assert;
+
 
 public class MobBossControllerTest {
     @Test(groups = {"unit"})
     public static void someTest() {
-        org.testng.Assert.assertTrue(true);
+        Assert.assertTrue(true);
     }
 
 }
